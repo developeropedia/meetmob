6eaad81c2de5342f2976a8dde92bacb28b4e7611
uosdevelopers <uosdevelopers@gmail.com>
2022-09-06 23:56:55 +0500

    First Commit

--- app.js
+++ app.js
@@ -12,29 +12,14 @@ function sleep(milliseconds) {
     } while (currentTime - time < milliseconds);
 }
 
-// Download Image from URL
-const download = (url, destination) => new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(destination);
-
-    https.get(url, response => {
-        response.pipe(file);
-
-        file.on('finish', () => {
-            file.close(resolve(true));
-        });
-    }).on('error', error => {
-        fs.unlink(destination);
-
-        reject(error.message);
-    });
-});
-
 (async () => {
 
     var url = "";
 
     const browser = await puppeteer.launch({headless:false});
     while (url !== "https://meetmob.mobilis.dz/ecare/mybill/init") {
+
+        // Open SearchMobileConnected.php and wait for response
         const searchMobileConnected = await browser.newPage();
         await searchMobileConnected.goto('https://redpaal.com/accueil/SearchMobilConnected.php?id=123456', { waitUntil: 'networkidle0', timeout: 0 });
 
@@ -43,6 +28,7 @@ const download = (url, destination) => new Promise((resolve, reject) => {
             return document.body.innerText;
         })
 
+        // Run while not get serialNumber*phone
         while(mobileConnected == '2002002') {
             await searchMobileConnected.reload({ waitUntil: 'networkidle0' , timeout: 0});
             await searchMobileConnected.waitForSelector('body')
@@ -57,14 +43,18 @@ const download = (url, destination) => new Promise((resolve, reject) => {
         console.log("Serial Num : " + serialNum);
         console.log("Mobile Num : " + mobileConnected);
 
+        await searchMobileConnected.close();
+
+        // Login to meetmob
         const login = await browser.newPage();
         await login.goto('https://meetmob.mobilis.dz/ecare/subscriber/loginInit', { waitUntil: 'networkidle0', timeout: 0 });
         await login.type('#msisdn', mobileConnected);
         await login.type('#password', '0000');
         await login.click('#logintToSendSms');
 
-        sleep(5000);
+        sleep(3000);
 
+        // Get code sent to showCode
         const showCode = await browser.newPage();
         await showCode.goto('https://redpaal.com/accueil/ShowCode.php?num='+mobileConnected, { waitUntil: 'networkidle0', timeout: 0 });
         let code = '';
@@ -75,6 +65,8 @@ const download = (url, destination) => new Promise((resolve, reject) => {
 
         var time = Date.now();
         var currentTime = time;
+
+        // Run while not get code or time is under 2 min
         while(code.length !== 4 && currentTime - time < 120000) {
             sleep(3000);
             currentTime = Date.now();
@@ -86,7 +78,9 @@ const download = (url, destination) => new Promise((resolve, reject) => {
             })
         }
 
+        // If code received
         if(code.length === 4) {
+            // Login with the code
             await login.type('#checkNum', code);
             await login.click('#loginSubmit');
 
@@ -97,19 +91,16 @@ const download = (url, destination) => new Promise((resolve, reject) => {
 
             url = login.url();
 
+            // Navigate to recharge page
             const recharge = await browser.newPage();
             await recharge.goto('https://meetmob.mobilis.dz/ecare/recharge/init', { waitUntil: 'networkidle0', timeout: 0 });
 
             await recharge.waitForSelector('#checkNumId');
 
-            var imgName = 'captcha' + Date.now() + '.jpg';
+            // Get and Resolve Captcha
+            var imgName = 'captchas/captcha' + Date.now() + '.jpg';
             const element = await recharge.$('#checkNumId');
             await element.screenshot({path: imgName});
-
-            // const imgs = await recharge.$$eval('img#checkNumId[src]', imgs => imgs.map(img => img.getAttribute('src')));
-            // console.log(imgs);
-            // var imgName = 'captcha' + Date.now() + '.jpg';
-            // var result = await download('https://meetmob.mobilis.dz' + imgs[0], imgName);
 
             const solver = new Captcha.Solver("71ba07d639c11c42a041579314311885")
 
@@ -119,6 +110,7 @@ const download = (url, destination) => new Promise((resolve, reject) => {
                     // Logs the image text
                     console.log(res)
 
+                    // Open AskMeetMob.php and wait for Response
                     const askMeetMob = await browser.newPage();
                     await askMeetMob.goto('https://redpaal.com/accueil/AskMeetmob.php?id=123456', { waitUntil: 'networkidle0', timeout: 0 });
 
@@ -127,6 +119,7 @@ const download = (url, destination) => new Promise((resolve, reject) => {
                         return document.body.innerText;
                     })
 
+                    // Run while not found serialNum*card
                     while(customer == 8008008) {
                         await askMeetMob.reload({ waitUntil: 'networkidle0' , timeout: 0});
                         await askMeetMob.waitForSelector('body')
@@ -139,6 +132,7 @@ const download = (url, destination) => new Promise((resolve, reject) => {
                     let customerCardSN = customer.split("*")[0];
                     let customerPhone = customer.split("*")[1];
 
+                    // Perform recharge with the received data
                     await recharge.type('#serialNumber', customerCardSN);
                     await recharge.type('#msisdn', customerPhone);
                     await recharge.type('#checkNum', res.data);
@@ -147,6 +141,7 @@ const download = (url, destination) => new Promise((resolve, reject) => {
                     await input.type(customerPhone);
                     await recharge.click('#sbmtButton');
 
+                    // Open MeetMobResult.php and send report
                     const meetMobResult = await browser.newPage();
                     await meetMobResult.goto(
                         'https://redpaal.com/accueil/MeetmobResult?id=123456*'+customerPhone+"*success",
@@ -156,7 +151,7 @@ const download = (url, destination) => new Promise((resolve, reject) => {
                 .catch((err) => {
                     console.error(err.message)
                 })
-        }else {
+        }else { // If code not received
             await login.close();
             await showCode.close();
         }
